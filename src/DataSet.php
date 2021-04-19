0b42af007b8d57cbcfece76eb444d14a8a74b02b
Alireza Kamali <arka1373@gmail.com>
2021-04-19 21:44:33 +0430

    New package

---
+++ src/DataSet.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Hooshid\Utils;
+
+
+class DataSet
+{
+    public static function serialize($data)
+    {
+        $dataSet = [];
+        $json = json_decode($data);
+        $action = "insert";
+        foreach ($json as $obj) {
+            // insert or update?
+            if ($obj->key == "id" and $obj->value) {
+                $action = "update";
+            }
+
+            if ($obj->key) {
+                $dataSet[$obj->key] = strip_tags($obj->value);
+            }
+        }
+
+        if ($action == "insert") {
+            unset($dataSet['id']);
+            $dataSet['created_at'] = now();
+            $dataSet['updated_at'] = now();
+        } else {
+            $dataSet['updated_at'] = now();
+        }
+
+        return $dataSet;
+    }
+}
