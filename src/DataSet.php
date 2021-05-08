7689bc2866160449a6ba46d83b6c78865ecd6f9e
Alireza Kamali <arka1373@gmail.com>
2021-05-08 22:06:24 +0430

    added multi select in DataSet.php

--- src/DataSet.php
+++ src/DataSet.php
@@ -2,6 +2,8 @@
 
 namespace Hooshid\Utils;
 
+use Illuminate\Support\Facades\DB;
+
 class DataSet
 {
     public static function serialize($data)
@@ -15,8 +17,41 @@ class DataSet
                 $action = "update";
             }
 
-            if ($obj->key) {
-                $dataSet[$obj->key] = strip_tags($obj->value);
+            // multi selects
+            if ($obj->type == "select" and gettype($obj->value) == "object" and $obj->value->multiple) {
+                $collection = DB::table($obj->value->save_to_db)
+                    ->where($obj->value->related_key, $dataSet['id'])
+                    ->select($obj->value->source_key)
+                    ->get()
+                    ->pluck($obj->value->source_key);
+
+                // tag deleted
+                $deleteValues = $collection->diff($obj->value->values);
+                if ($deleteValues->all()) {
+                    foreach ($deleteValues->all() as $id) {
+                        DB::table($obj->value->save_to_db)
+                            ->where($obj->value->related_key, $dataSet['id'])
+                            ->where($obj->value->source_key, $id)
+                            ->delete();
+                    }
+                }
+
+                // insert new tags
+                $insertCol = collect($obj->value->values);
+                $insertValues = $insertCol->diff($collection);
+                if ($insertValues->all()) {
+                    foreach ($insertValues->all() as $id) {
+                        DB::table($obj->value->save_to_db)->insert([
+                            $obj->value->related_key => $dataSet['id'],
+                            $obj->value->source_key => $id,
+                            'created_at' => now(),
+                            'updated_at' => now()
+                        ]);
+                    }
+                }
+            } // insert key & value in array
+            elseif ($obj->key) {
+                $dataSet[$obj->key] = trim(strip_tags($obj->value));
             }
         }
 
